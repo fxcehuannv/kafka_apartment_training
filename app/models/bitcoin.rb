34f1fc4a6b387e2f6b2722fd33b416c22bbe91e2
Huan Nguyen <nguyenvanhuan243@gmail.com>
2023-04-27 00:00:32 +0700

    lock

--- app/models/bitcoin.rb
+++ app/models/bitcoin.rb
@@ -13,21 +13,22 @@ class Bitcoin < ApplicationRecord
     5.times do
       threads << Thread.new do
         Bitcoin.transaction do
-          bitcoin = Bitcoin.lock(true).find_by(name: 'Bitcoin')
+          bitcoin = Bitcoin.find_by(name: 'Bitcoin')
+          bitcoin.lock!
           bitcoin.count += 1
           bitcoin.save!
         end
       end
     end
     threads.each(&:join)
-    puts "################ Bitcoin #{Bitcoin.last.count == 5} ###################"
+    puts "################ Bitcoin (#{Bitcoin.last.count}) #{Bitcoin.last.count == 5} ###################"
   end
 
   def update_bitcoin_thread
     Bitcoin.find_by(name: 'Bitcoin').update(count: 0)
     sleep 5
     threads = []
-    5.times do
+    1000.times do
       threads << Thread.new do
         bitcoin = Bitcoin.find_by(name: 'Bitcoin')
         bitcoin.count += 1
